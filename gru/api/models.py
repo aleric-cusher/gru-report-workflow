71b9a62f3be8489610b2194cf2c0b06eb74b5f92
Aleric Cusher <alericcusher25@gmail.com>
2023-11-17 00:55:23 +0530

    Adds fields for managing superagi runs in models and refactors its tests

--- gru/api/models.py
+++ gru/api/models.py
@@ -7,7 +7,13 @@ class ContactLeads(models.Model):
     phone = models.CharField(max_length=15)
     message = models.CharField(max_length=300, blank=True, null=True)
 
+    # SuperAGI agent config fields
     company_name = models.CharField(max_length=50)
     company_website = models.URLField()
     industry = models.CharField(max_length=20)
     goals = models.CharField(max_length=200)
+
+    # SuperAGI agent run fields
+    superagi_run_complete = models.BooleanField(blank=True, default=False)
+    agent_id = models.PositiveIntegerField(blank=True, null=True)
+    run_id = models.PositiveIntegerField(blank=True, null=True)
--- gru/api/tests/test_models.py
+++ gru/api/tests/test_models.py
@@ -1,49 +1,44 @@
 from django.test import TestCase
 from api.models import ContactLeads
+from django.forms import model_to_dict
 
 
 class ContactLeadsModelTest(TestCase):
     def test_all_fields(self):
-        ContactLeads.objects.create(
-            name="John Doe",
-            email="john.doe@example.com",
-            phone="1234567890",
-            message="Test message",
-            company_name="Test Company",
-            company_website="http://www.testcompany.com",
-            industry="Test Industry",
-            goals="Test Goals",
-        )
+        expected_dict = {
+            "name": "John Doe",
+            "email": "john.doe@example.com",
+            "phone": "1234567890",
+            "message": "Test message",
+            "company_name": "Test Company",
+            "company_website": "http://www.testcompany.com",
+            "industry": "Test Industry",
+            "goals": "Test Goals",
+            "superagi_run_complete": True,
+            "agent_id": 393928,
+            "run_id": 123,
+        }
 
-        contact_lead = ContactLeads.objects.get(name="John Doe")
+        ContactLeads.objects.create(**expected_dict)
 
-        self.assertEqual(contact_lead.name, "John Doe")
-        self.assertEqual(contact_lead.email, "john.doe@example.com")
-        self.assertEqual(contact_lead.phone, "1234567890")
-        self.assertEqual(contact_lead.message, "Test message")
-        self.assertEqual(contact_lead.company_name, "Test Company")
-        self.assertEqual(contact_lead.company_website, "http://www.testcompany.com")
-        self.assertEqual(contact_lead.industry, "Test Industry")
-        self.assertEqual(contact_lead.goals, "Test Goals")
+        contact_lead = model_to_dict(ContactLeads.objects.get(name="John Doe"))
+
+        for each in expected_dict.keys():
+            self.assertEquals(contact_lead[each], expected_dict[each])
 
     def test_required_fields(self):
-        # Test creating a ContactLeads instance with only required fields
-        ContactLeads.objects.create(
-            name="Jane Doe",
-            phone="1234567890",
-            company_name="Test Company",
-            company_website="http://www.testcompany.com",
-            industry="Test Industry",
-            goals="Test Goals",
-        )
+        expected_dict = {
+            "name": "Jane Doe",
+            "phone": "1234567890",
+            "company_name": "Test Company",
+            "company_website": "http://www.testcompany.com",
+            "industry": "Test Industry",
+            "goals": "Test Goals",
+        }
 
-        contact_lead = ContactLeads.objects.get(name="Jane Doe")
+        ContactLeads.objects.create(**expected_dict)
 
-        self.assertEqual(contact_lead.name, "Jane Doe")
-        self.assertIsNone(contact_lead.email)
-        self.assertEqual(contact_lead.phone, "1234567890")
-        self.assertIsNone(contact_lead.message)
-        self.assertEqual(contact_lead.company_name, "Test Company")
-        self.assertEqual(contact_lead.company_website, "http://www.testcompany.com")
-        self.assertEqual(contact_lead.industry, "Test Industry")
-        self.assertEqual(contact_lead.goals, "Test Goals")
+        contact_lead = model_to_dict(ContactLeads.objects.get(name="Jane Doe"))
+
+        for each in expected_dict.keys():
+            self.assertEquals(contact_lead[each], expected_dict[each])
