39b75f6d34a3331a9498a35561eaa9faa725cc30
Aleric Cusher <alericcusher25@gmail.com>
2023-11-16 13:33:06 +0530

    Adds contact lead model for capturing leads in api app

--- gru/api/models.py
+++ gru/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactLeads(models.Model):
+    name = models.CharField(max_length=60)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=15)
+    message = models.CharField(max_length=300, blank=True, null=True)
+
+    company_name = models.CharField(max_length=50)
+    company_website = models.URLField()
+    industry = models.CharField(max_length=20)
+    goals = models.CharField(max_length=200)
