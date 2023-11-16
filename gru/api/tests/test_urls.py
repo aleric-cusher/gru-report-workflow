9c911fc4c2248d5837cea329e59759264a9fbf92
Aleric Cusher <alericcusher25@gmail.com>
2023-11-16 16:02:52 +0530

    Adds api endpoint contact lead and its test

---
+++ gru/api/tests/test_urls.py
@@ -0,0 +1,9 @@
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+from api import views
+
+
+class TestUrls(SimpleTestCase):
+    def test_contact_lead_url(self):
+        url = reverse("contact-lead")
+        self.assertEquals(resolve(url).func, views.contact_lead)
