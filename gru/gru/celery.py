5dfd447469b48e986c8569ead2d683f26f13b460
Aleric Cusher <alericcusher25@gmail.com>
2023-11-16 18:46:53 +0530

    Adds celery.py file in django project for handling superAGI workflows

---
+++ gru/gru/celery.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+
+# create a Celery instance and configure it using the settings from Django
+app = Celery("superAGI_workflow")
+
+# Update config.
+app.conf.update(
+    result_backend=os.environ.get("CELERY_RESULT_BACKEND"),
+    broker_url=os.environ.get("CELERY_BROKER_URL"),
+)
+
+# Auto-discover tasks in all installed apps
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
