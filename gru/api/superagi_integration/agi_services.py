82ced823bfc6d92c266d6d5bdb1d139841ca8a97
Aleric Cusher <alericcusher25@gmail.com>
2023-11-18 01:14:25 +0530

    Adds method signatures for the following methods in AGIServices class: - _generate_agent_config - create_agent - run_agent - create_and_run_agent - pause_agent - resume_agent - check_run_status
    Note: Implementation and tests for these methods is pending

--- gru/api/superagi_integration/agi_services.py
+++ gru/api/superagi_integration/agi_services.py
@@ -1,4 +1,5 @@
 from .agi_client_initializer import AGIClientInitializer
+from superagi_client import AgentConfig
 
 
 class AGIServices:
@@ -12,3 +13,24 @@ class AGIServices:
             )
 
         self.client = client_initializer_instance.get_client()
+
+    def _generate_agent_config(self) -> AgentConfig:
+        pass
+
+    def create_agent(self, data: dict) -> int:
+        pass
+
+    def run_agent(self, agent_id: int) -> int:
+        pass
+
+    def create_and_run_agent(self, data: dict) -> list[int, int]:
+        pass
+
+    def pause_agent(self, agent_id: int, run_id: int) -> None:
+        pass
+
+    def resume_agent(self, agent_id: int, run_id: int) -> None:
+        pass
+
+    def check_run_status(self, agent_id: int, run_id: int = None):
+        pass
