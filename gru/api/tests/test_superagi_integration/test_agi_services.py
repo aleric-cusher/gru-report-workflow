ccd84af0ee74a5f8e9f610a079cd42ff17941162
Aleric Cusher <alericcusher25@gmail.com>
2023-11-18 01:53:04 +0530

    Refactors AGIServices's tests to reduce code duplication

--- gru/api/tests/test_superagi_integration/test_agi_services.py
+++ gru/api/tests/test_superagi_integration/test_agi_services.py
@@ -12,13 +12,23 @@ class MockClient:
 
 
 class TestAGIServices(SimpleTestCase):
-    @patch(
-        "api.superagi_integration.agi_client_initializer.AGIClientInitializer.get_client"
-    )
-    def test_valid_creation(self, mock_get_client):
-        mock_get_client.return_value = MockClient()
+    def setUp(self) -> None:
+        self.mock_get_client_patcher = patch(
+            "api.superagi_integration.agi_client_initializer.AGIClientInitializer.get_client"
+        )
+        self.mock_get_client = self.mock_get_client_patcher.start()
+        self.mock_get_client.return_value = MockClient()
 
-        services = AGIServices(AGIClientInitializer("test_api_key", "http://test.com"))
+        self.api_key = "test_api_key"
+        self.host = "http://test.com"
+
+        self.client_initializer = AGIClientInitializer(self.api_key, self.host)
+
+    def tearDown(self) -> None:
+        self.mock_get_client_patcher.stop()
+
+    def test_valid_creation(self):
+        services = AGIServices(self.client_initializer)
 
         self.assertIsInstance(services.client, MockClient)
 
@@ -26,12 +36,7 @@ class TestAGIServices(SimpleTestCase):
         with self.assertRaises(TypeError):
             services = AGIServices("AGIClientInitializer()")
 
-    @patch(
-        "api.superagi_integration.agi_client_initializer.AGIClientInitializer.get_client"
-    )
-    def test_private_generate_agent_config_method(self, mock_get_client):
-        mock_get_client.return_value = MockClient()
-
+    def test_private_generate_agent_config_method(self):
         data_dict = {
             "company_name": "Test Company",
             "company_website": "www.testwebsite.com",
@@ -39,7 +44,7 @@ class TestAGIServices(SimpleTestCase):
             "goals": "Testing Goals",
         }
 
-        services = AGIServices(AGIClientInitializer("test_api_key", "http://test.com"))
+        services = AGIServices(self.client_initializer)
         config = services._generate_agent_config(data_dict)
 
         self.assertIsInstance(config, AgentConfig)
