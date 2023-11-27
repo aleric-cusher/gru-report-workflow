bac26eb72c0a8087f9732967171a7155f2ecc88f
Aleric Cusher(Mac) <alericcusher25@gmail.com>
2023-11-27 23:53:19 +0530

    Adds a pre_save signal to process reports and mail them and its tests

--- gru/api/signals.py
+++ gru/api/signals.py
@@ -1,11 +1,27 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
-from .tasks import add_agent_workflow
+from .tasks import add_agent_workflow, process_and_email_report
 from .models import ContactLeads
 
 
 @receiver(post_save, sender=ContactLeads)
-def on_contact_lead_save(sender, instance, created, **kwargs):
+def on_contact_lead_save(
+    sender: ContactLeads, instance: ContactLeads, created: bool, **kwargs
+):
     if created:
         add_agent_workflow.delay(instance)
+
+
+@receiver(pre_save, sender=ContactLeads)
+def on_superagi_run_complete_update(
+    sender: ContactLeads, instance: ContactLeads, **kwargs
+):
+    if instance.pk is not None and ContactLeads.objects.filter(pk=instance.pk).exists():
+        snapshot = ContactLeads.objects.get(pk=instance.pk)
+        if (
+            (not snapshot.superagi_run_complete)
+            and instance.superagi_run_complete
+            and instance.superagi_resource
+        ):
+            process_and_email_report.delay(instance)
--- gru/api/tests/test_signals.py
+++ gru/api/tests/test_signals.py
@@ -1,12 +1,16 @@
 from django.test import TestCase
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import Signal
 from unittest import mock
 from api.models import ContactLeads
-from api.signals import on_contact_lead_save
+from api.signals import on_contact_lead_save, on_superagi_run_complete_update
 
 
 class ContactLeadsSignalTest(TestCase):
+    def setUp(self) -> None:
+        post_save.disconnect(on_contact_lead_save, sender=ContactLeads)
+        pre_save.disconnect(on_superagi_run_complete_update, sender=ContactLeads)
+
     def test_on_contact_lead_save_signal(self):
         contact_lead_instance = ContactLeads.objects.create(name="Test Contact Lead")
 
@@ -23,3 +27,21 @@ class ContactLeadsSignalTest(TestCase):
         # Disconnect the signal to avoid interference with other tests
         post_save.disconnect(on_contact_lead_save, sender=ContactLeads)
         mock_add_agent_workflow.assert_called_once_with(contact_lead_instance)
+
+    def test_on_superagi_run_complete_update(self):
+        contact_lead_instance = ContactLeads.objects.create(name="Test Contact Lead")
+
+        with mock.patch(
+            "api.signals.process_and_email_report"
+        ) as mock_process_and_email_report:
+            with mock.patch(
+                "api.signals.process_and_email_report.delay",
+                mock_process_and_email_report,
+            ):
+                pre_save.connect(on_superagi_run_complete_update, sender=ContactLeads)
+                contact_lead_instance.superagi_run_complete = True
+                contact_lead_instance.superagi_resource = "https://testresource.com"
+                contact_lead_instance.save()
+
+        pre_save.disconnect(on_superagi_run_complete_update, sender=ContactLeads)
+        mock_process_and_email_report.assert_called_once_with(contact_lead_instance)
